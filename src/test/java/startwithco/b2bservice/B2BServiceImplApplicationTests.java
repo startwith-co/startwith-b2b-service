40eba029b4777148f081c3820ecb5a5c4bd6ca93
lynjun <lynjun97@naver.com>
2025-05-02 21:11:35 +0900

    test: 클래스명 수정

--- src/test/java/startwithco/b2bservice/B2BServiceImplApplicationTests.java
+++ src/test/java/startwithco/b2bservice/B2BServiceImplApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class B2bServiceApplicationTests {
+class B2BServiceImplApplicationTests {
 
     @Test
     void contextLoads() {
